91eeb953fdc522ed4175b83a1a2dd8c9c3e2fc9b
cotton <chinoknct@gmail.com>
2023-08-12 13:32:02 +0900

    Add sqlboiler settings

--- packages/backend/tools/tools.go
+++ packages/backend/tools/tools.go
@@ -6,6 +6,7 @@ package tools
 // バージョン追跡を行いたいgoベースのツールをこのインポートステートメントに記載する
 // https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
 import (
-	_ "github.com/pressly/goose/v3/cmd/goose"
-	_ "github.com/volatiletech/sqlboiler/v4"
+    _ "github.com/pressly/goose/v3/cmd/goose"
+    _ "github.com/volatiletech/sqlboiler/v4"
+    _ "github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-mysql"
 )
