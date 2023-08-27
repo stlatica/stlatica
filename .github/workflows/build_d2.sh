5ba57812f1612a59a83bbafa35b7e4ea7c50fa3f
Percy <shin.parshi@gmail.com>
2023-08-27 23:20:09 +0900

    README.mdを編集

--- .github/workflows/build_d2.sh
+++ .github/workflows/build_d2.sh
@@ -1,7 +1,7 @@
 for file_dir in "${@}"; do
     case "$file_dir" in 
         *.d2)
-        d2 --theme=300 --dark-theme=200 -l elk --pad 0 "$file_dir";;
+        d2 --layout=elk "$file_dir";;
     esac
 
 done
