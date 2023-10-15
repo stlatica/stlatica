9e6d4911f2f4fc25bbe2ffee29f520315795b809
para7 <cometpara7@gmail.com>
2023-10-15 16:23:04 +0900

    update template

--- packages/frontend/src/components/button/CancelButton/CancelButton.test.tsx
+++ packages/frontend/src/components/button/CancelButton/CancelButton.test.tsx
@@ -3,7 +3,9 @@ import { render } from "@testing-library/react";
 import { Sample1 } from "./sample";
 import { describe, expect, test } from "vitest";
 
-test("snapshot", () => {
-  const { asFragment } = render(<Sample1 />);
-  expect(asFragment()).toMatchSnapshot();
+describe("snapshot", () => {
+  test("snapshot", () => {
+    const { asFragment } = render(<Sample1 />);
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
--- packages/frontend/src/components/button/SubmitButton/SubmitButton.test.tsx
+++ packages/frontend/src/components/button/SubmitButton/SubmitButton.test.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { Sample1 } from "./sample";
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 
-test("snapshot", () => {
-  const { asFragment } = render(<Sample1 />);
-  expect(asFragment()).toMatchSnapshot();
+describe("snapshot", () => {
+  test("snapshot", () => {
+    const { asFragment } = render(<Sample1 />);
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
