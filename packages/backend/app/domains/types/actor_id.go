436b63bd18b7dbedd4f6939fdc96588adffa2a40
cotton <chinoknct@gmail.com>
2023-11-04 15:08:15 +0900

    Fix lint

--- packages/backend/app/domains/types/actor_id.go
+++ packages/backend/app/domains/types/actor_id.go
@@ -2,8 +2,10 @@ package types
 
 import "github.com/oklog/ulid/v2"
 
+// ActorID is the type for actor ID.
 type ActorID ulid.ULID
 
+// NewActorID returns a new actor ID.
 func NewActorID() ActorID {
 	return ActorID(ulid.Make())
 }
