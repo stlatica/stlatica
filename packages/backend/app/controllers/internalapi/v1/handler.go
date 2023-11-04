9fc8adb45349dc995eecac3f5e0f37ab0b72b9b7
cotton <chinoknct@gmail.com>
2023-11-04 17:28:33 +0900

    Update controller process for user acquisition endpoint

--- packages/backend/app/controllers/internalapi/v1/handler.go
+++ packages/backend/app/controllers/internalapi/v1/handler.go
@@ -5,29 +5,37 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/stlatica/stlatica/packages/backend/app/controllers/internalapi/v1/openapi"
+	"github.com/stlatica/stlatica/packages/backend/app/usecases/actors"
 )
 
 type handler struct {
 	*userController
 }
 
-func newHandler() openapi.ServerInterface {
+func newHandler(initContent ControllerInitContents) openapi.ServerInterface {
 	return &handler{
-		userController: &userController{},
+		userController: &userController{
+			actorUseCase: initContent.ActorUseCase,
+		},
 	}
 }
 
 // RegisterHandlers adds each server route to the EchoRouter.
-func RegisterHandlers(server *echo.Echo) {
-	handler := newHandler()
+func RegisterHandlers(initContent ControllerInitContents, server *echo.Echo) {
+	handler := newHandler(initContent)
 	openapi.RegisterHandlers(server, handler)
 }
 
 // GetUser is the handler for GET /users/{user_id}, ServerInterface implementation.
 func (h *handler) GetUser(ectx echo.Context, userID string) error {
-	response, err := h.userController.GetUser(userID)
+	response, err := h.userController.GetUser(ectx, userID)
 	if err != nil {
 		return err
 	}
 	return ectx.JSON(http.StatusOK, response)
+}
+
+// ControllerInitContents is the struct to hold the dependencies for the controller.
+type ControllerInitContents struct {
+	ActorUseCase actors.ActorUseCase
 }
