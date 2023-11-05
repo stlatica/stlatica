3030733b4ce861567dac80f7830db5ac9aa66243
nobuchi <hanazawa9to13ctrl@gmail.com>
2023-11-06 03:05:33 +0900

    components/PlatCellのtailwind化

--- packages/frontend/src/components/common/PlatCell/PlatCell.stories.tsx
+++ packages/frontend/src/components/common/PlatCell/PlatCell.stories.tsx
@@ -38,7 +38,7 @@ Story2.storyName = "複数行のPlat";
 
 export const Story3: Story = () => {
   return (
-    <div style={{ width: "310px" }}>
+    <div className="w-[310px]">
       <PlatCell
         content="nobuchiさんのパナソニックグループ プログラミングコンテスト2023（AtCoder Beginner Contest 326）での成績：4526位, パフォーマンス：670相当, レーティング：789→777 (-12) "
         favoriteCount={8}
@@ -54,7 +54,7 @@ Story3.storyName = "width=310";
 
 export const Story4: Story = () => {
   return (
-    <div style={{ width: "700px" }}>
+    <div className="w-[700px]">
       <PlatCell
         content="nobuchiさんのパナソニックグループ プログラミングコンテスト2023（AtCoder Beginner Contest 326）での成績：4526位, パフォーマンス：670相当, レーティング：789→777 (-12) "
         favoriteCount={8}
@@ -70,7 +70,7 @@ Story4.storyName = "width=700";
 
 export const Story5: Story = () => {
   return (
-    <div style={{ width: "1000px" }}>
+    <div className="w-1000px]">
       <PlatCell
         content="nobuchiさんのパナソニックグループ プログラミングコンテスト2023（AtCoder Beginner Contest 326）での成績：4526位, パフォーマンス：670相当, レーティング：789→777 (-12) "
         favoriteCount={8}
