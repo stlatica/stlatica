a913625c2787f3ac22ad150f1c0124cfa096af74
LRun <96586585+LR-nemusuke@users.noreply.github.com>
2023-11-12 22:28:12 +0900

    complete icon display implementation

---
+++ packages/frontend/src/components/common/UserIcon/UserIcon.test.tsx
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import { describe, expect, test } from "vitest";
+
+import { Story1 } from "./UserIcon.stories";
+
+describe("snapshot", () => {
+  test("snapshot", () => {
+    const { asFragment } = render(<Story1 />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
