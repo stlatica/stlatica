08c62d7261f5803fec7a42e10cb28f9196c89a6f
para7 <cometpara7@gmail.com>
2023-11-19 21:54:18 +0900

    エラー確認しつつ整え

--- packages/frontend/src/components/common/UserIcon/UserIcon.tsx
+++ packages/frontend/src/components/common/UserIcon/UserIcon.tsx
@@ -9,9 +9,6 @@ type UserIconProps = {
   readonly size: number;
 };
 
-const cropStyle = {
-  borderRadius: "50%",
-};
 /**
  * user icon
  */
@@ -19,7 +16,7 @@ export const UserIcon: React.FC<UserIconProps> = (props) => {
   const { iconImage, size } = props;
   return (
     <div style={{ height: size, width: size }}>
-      <img alt="icon" src={iconImage} style={cropStyle} />
+      <img alt="icon" className="rounded-full" src={iconImage} />
     </div>
   );
 };
