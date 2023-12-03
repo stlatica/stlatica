3c2afbdc39e3fb9cc61fce99092f528b9c634358
polyester <56536708+polyester-CTRL@users.noreply.github.com>
2023-12-03 23:06:21 +0900

    コメント追加、パッケージ名修正

--- packages/backend/app/domains/plats/get.go
+++ packages/backend/app/domains/plats/get.go
@@ -1,4 +1,4 @@
-package actors
+package plats
 
 import (
 	"context"
--- packages/backend/app/domains/plats/ports/input_port.go
+++ packages/backend/app/domains/plats/ports/input_port.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stlatica/stlatica/packages/backend/app/domains/types"
 )
 
+// PlatGetInPort is the interface for getting plat.
 type PlatGetInPort interface {
 	// GetPlat returns Plat. 
 	GetPlat(ctx context.Context, platID types.PlatID) (*entities.Plat, error)
