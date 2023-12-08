c6a9022c6724b7fc9ca7aa9d7cd28d2337f4dddf
LRun <96586585+LR-nemusuke@users.noreply.github.com>
2023-12-08 23:07:08 +0900

    fixed snapshot

--- packages/frontend/src/components/common/UserIcon/UserIcon.test.tsx
+++ packages/frontend/src/components/common/UserIcon/UserIcon.test.tsx
@@ -5,15 +5,15 @@ import { describe, expect, test } from "vitest";
 import { Story1, Story2, Story3 } from "./UserIcon.stories";
 
 describe("snapshot", () => {
-  test("snapshot", () => {
+  test("snapshot1", () => {
     const { asFragment } = render(<Story1 />);
     expect(asFragment()).toMatchSnapshot();
   });
-  test("snapshot", () => {
+  test("snapshot2", () => {
     const { asFragment } = render(<Story2 />);
     expect(asFragment()).toMatchSnapshot();
   });
-  test("snapshot", () => {
+  test("snapshot3", () => {
     const { asFragment } = render(<Story3 />);
     expect(asFragment()).toMatchSnapshot();
   });
