035418b7b23fd2724a9c6b8a7c0b6d5897bb583b
nobuchi <hanazawa9to13ctrl@gmail.com>
2024-01-02 04:05:09 +0900

    余分な{}の削除

--- packages/frontend/src/app/EditProfile/page.tsx
+++ packages/frontend/src/app/EditProfile/page.tsx
@@ -22,9 +22,9 @@ export default function Home() {
           </div>
           <div className="mt-12 flex justify-around">
             {/* Assuming SubmitButton and CancelButton are styled components, you would need to pass the class names instead of the style prop. */}
-            <SubmitButton className="w-[85px]">Save</SubmitButton>{" "}
+            <SubmitButton className="w-[85px]">Save</SubmitButton>
             {/* Adjust the width as needed */}
-            <CancelButton className="w-[85px]">Cancel</CancelButton>{" "}
+            <CancelButton className="w-[85px]">Cancel</CancelButton>
             {/* Adjust the width as needed */}
           </div>
         </div>
