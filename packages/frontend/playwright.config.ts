3dac94d16b4f5914dc0dde0d79ab03cd336e8427
para7 <cometpara7@gmail.com>
2024-01-07 01:40:52 +0900

    VRT 動作成功

---
+++ packages/frontend/playwright.config.ts
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { PlaywrightTestConfig } from "@playwright/test";
+
+export default {
+  webServer: {
+    command: "pnpm ladle build && pnpm ladle preview -p 61000",
+    // command:
+    //   process.env.TYPE === "dev" ? "pnpm ladle serve" : "pnpm ladle build && pnpm ladle preview",
+    url: `http://localhost:61000/stlatica`,
+  },
+  testDir: "playwright",
+  reporter: "html",
+  outputDir: "playwright/results",
+} satisfies PlaywrightTestConfig;
