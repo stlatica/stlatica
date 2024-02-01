2799ecfcfa80e723c6b8e5083bf70ebecf820b93
LRun <96586585+LRun99@users.noreply.github.com>
2024-02-02 01:26:49 +0900

    made login scene base, fixed login button
    made login scene base without text editor.
    fixed login button, changed to use submit button.

--- packages/frontend/src/components/others/LoginScene/LoginScene.tsx
+++ packages/frontend/src/components/others/LoginScene/LoginScene.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { LoginButton } from "@/components/button/LoginButton";
+
 type LoginSceneProps = {
   // children: React.ReactNode;
 };
@@ -8,5 +10,22 @@ type LoginSceneProps = {
  *
  */
 export const LoginScene: React.FC<LoginSceneProps> = () => {
-  return <div>LoginScene</div>;
+  return (
+    <div className="grid h-full w-full place-content-center space-y-5 bg-gray-200">
+      <div className="text-5xl">Stlatica</div>
+      {/* <div className="">
+        <TextEditor defaultValue="" maxlength={32} title="mail address" />
+      </div> */}
+      {/* <div className="">
+        <TextEditor defaultValue="" maxlength={32} title="password" />
+      </div> */}
+      <div className="">
+        <LoginButton
+          onClick={() => {
+            console.log("clicked login button");
+          }}
+        />
+      </div>
+    </div>
+  );
 };
