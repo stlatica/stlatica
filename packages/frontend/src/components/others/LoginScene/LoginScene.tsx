6e14f0e8656cdbdee2cc11d44569c6bfb8eb32e4
LRun <96586585+LRun99@users.noreply.github.com>
2024-02-02 17:15:59 +0900

    changed text editor height

--- packages/frontend/src/components/others/LoginScene/LoginScene.tsx
+++ packages/frontend/src/components/others/LoginScene/LoginScene.tsx
@@ -14,10 +14,10 @@ export const LoginScene: React.FC<LoginSceneProps> = () => {
   return (
     <div className="grid h-full w-full place-content-center space-y-3 bg-gray-800">
       <div className="flex justify-center text-5xl">Stlatica</div>
-      <div className="h-25 w-80 bg-gray-800 p-6 text-white">
+      <div className="h-24 w-80 bg-gray-800 p-6 text-white">
         <TextEditor defaultValue="" maxlength={32} title="mail address" />
       </div>
-      <div className="h-25 w-80 bg-gray-800 p-6 text-white">
+      <div className="h-24 w-80 bg-gray-800 p-6 text-white">
         <TextEditor defaultValue="" maxlength={32} title="password" />
       </div>
       <div className="flex justify-center">
