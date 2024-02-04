98043ca3fe3e37b54ba228bd53df431538bb4039
polyester <56536708+polyester-CTRL@users.noreply.github.com>
2024-02-04 17:36:13 +0900

    plat作成のdomain layerを実装

--- packages/backend/app/domains/plats/ports/input_port.go
+++ packages/backend/app/domains/plats/ports/input_port.go
@@ -12,3 +12,9 @@ type PlatGetInPort interface {
 	// GetPlat returns Plat.
 	GetPlat(ctx context.Context, platID types.PlatID) (*entities.Plat, error)
 }
+
+// PlatPostInport is the interface for posting plat.
+type PlatPostInPort interface {
+	// PostPlat returns Plat.
+	PostPlat(ctx context.Context, ActorID types.ActorID, content string) (*entities.Plat, error)
+}
---
+++ packages/backend/app/domains/plats/post.go
@@ -0,0 +1,22 @@
+package plats
+
+import (
+	"context"
+
+	"github.com/stlatica/stlatica/packages/backend/app/domains/entities"
+	"github.com/stlatica/stlatica/packages/backend/app/domains/plats/ports"
+	"github.com/stlatica/stlatica/packages/backend/app/domains/types"
+)
+
+// PlatPoster is the interface for posting plat.
+type PlatPoster interface {
+	// PostPlat returns new plat.
+	PostPlat(ctx context.Context, actorID types.ActorID, content string, inPort ports.PlatPostInPort) (*entities.Plat, error)
+}
+
+type platPoster struct{}
+
+func (g *platPoster) PostPlat(ctx context.Context, actorID types.ActorID,
+	content string, inPort ports.PlatPostInPort) (*entities.Plat, error) {
+	return inPort.PostPlat(ctx, actorID, content)
+}
