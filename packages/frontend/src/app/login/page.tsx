4638ab6ee9ddc8314a49f9c668eb6a06ea8fd31d
LRun <96586585+LRun99@users.noreply.github.com>
2024-02-08 02:04:08 +0900

    changed to use margin

--- packages/frontend/src/app/login/page.tsx
+++ packages/frontend/src/app/login/page.tsx
@@ -5,17 +5,19 @@ import { TextEditor } from "@/components/common/TextEditor";
 
 export default function LoginScene() {
   return (
-    <div className="grid h-full w-full place-content-center space-y-3 bg-gray-800">
-      <div className="flex justify-center text-5xl">Stlatica</div>
-      <div className="h-24 w-80 bg-gray-800 p-6 text-white">
-        <TextEditor defaultValue="" maxlength={32} title="mail address" />
+    <main className="h-[100vh]">
+      <div className="grid h-full w-full grid-cols-1 content-center space-y-6 bg-gray-800">
+        <div className="flex justify-center text-5xl">Stlatica</div>
+        <div className="mx-[25%] bg-gray-800 text-white">
+          <TextEditor defaultValue="" maxlength={32} title="mail address" />
+        </div>
+        <div className="mx-[25%] bg-gray-800 text-white">
+          <TextEditor defaultValue="" maxlength={32} title="password" />
+        </div>
+        <div className="flex justify-center">
+          <LoginButton />
+        </div>
       </div>
-      <div className="h-24 w-80 bg-gray-800 p-6 text-white">
-        <TextEditor defaultValue="" maxlength={32} title="password" />
-      </div>
-      <div className="flex justify-center">
-        <LoginButton />
-      </div>
-    </div>
+    </main>
   );
 }
