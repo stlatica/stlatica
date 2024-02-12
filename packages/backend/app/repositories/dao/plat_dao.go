1116fadfcfa4f6740a324ed1767c0cf90b3d394d
polyester <56536708+polyester-CTRL@users.noreply.github.com>
2024-02-12 20:30:51 +0900

    platを戻り値に追加

--- packages/backend/app/repositories/dao/plat_dao.go
+++ packages/backend/app/repositories/dao/plat_dao.go
@@ -13,7 +13,7 @@ import (
 type PlatDAO interface {
 	// GetPlat returns plat.
 	GetPlat(ctx context.Context, platID types.PlatID) (*domainentities.Plat, error)
-	CreatePlat(ctx context.Context, ActorID types.ActorID, content string) (error)
+	CreatePlat(ctx context.Context, ActorID types.ActorID, content string) (*domainentities.Plat, error)
 }
 
 // NewPlatDAO returns PlatDAO.
@@ -42,13 +42,19 @@ func (dao *platDAO) GetPlat(ctx context.Context, platID types.PlatID) (*domainen
 	}, nil
 }
 
-func (dao *platDAO) CreatePlat(ctx context.Context, actorID types.ActorID, content string) (error) {
-	plat := entities.Plat {
+func (dao *platDAO) CreatePlat(ctx context.Context, actorID types.ActorID, content string) (
+	*domainentities.Plat, error) {
+	plat := entities.Plat{
 		PlatID:    types.NewPlatID(),
 		ActorID:   actorID,
 		Content:   content,
 		CreatedAt: types.UnixTime(0),
 	}
 	err := plat.Insert(ctx, dao.ctxExecutor, boil.Infer())
-	return err
+	return &domainentities.Plat{
+		PlatID:    plat.PlatID,
+		ActorID:   plat.ActorID,
+		Content:   plat.Content,
+		CreatedAt: plat.CreatedAt,
+	}, err
 }
