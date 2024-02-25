2cf0e6d3a1714f4e9f281c1fe39f4a5e8771e011
LRun <96586585+LRun99@users.noreply.github.com>
2024-02-25 22:31:54 +0900

    made snapshot

--- packages/frontend/src/app/login/page.tsx
+++ packages/frontend/src/app/login/page.tsx
@@ -6,20 +6,19 @@ import { TextEditor } from "@/components/common/TextEditor";
 export default function LoginScene() {
   return (
     <main className="flex h-[100vh] w-[100vw] justify-center bg-gray-800">
-      <div className="flex w-[50%] min-w-[400px] max-w-[600px] flex-col">
-        <div className="mt-[50%] space-y-6">
-          <div className="flex justify-center text-5xl">Stlatica</div>
-          <div className="bg-gray-800 text-white">
-            <TextEditor defaultValue="" maxlength={32} title="mail address" />
-          </div>
-          <div className="bg-gray-800 text-white">
-            <TextEditor defaultValue="" maxlength={32} title="password" />
-          </div>
-          <div className="flex justify-center">
-            <LoginButton />
-          </div>
+      <div className="flex w-[80%] min-w-[300px] max-w-[800px] flex-col justify-center gap-6">
+        <div className="flex justify-center text-5xl">Stlatica</div>
+        <div className="bg-gray-800 text-white">
+          <TextEditor defaultValue="" maxLength={32} title="mail address" />
+        </div>
+        <div className="bg-gray-800 text-white">
+          <TextEditor defaultValue="" maxLength={32} title="password" />
+        </div>
+        <div className="flex justify-center">
+          <LoginButton />
         </div>
       </div>
     </main>
   );
 }
+
