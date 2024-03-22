a227789c2cc25f72b7ac5dd9716060b56b77db34
para7 <cometpara7@gmail.com>
2024-03-22 21:16:01 +0900

    型生成がエラーで失敗している状態

--- packages/front-mock/src/main.ts
+++ packages/front-mock/src/main.ts
@@ -1,8 +1,25 @@
-const express = require("express");
+import express from "express";
 const app = express();
 const port = 3000;
+import type * as MyTypes from "./schema";
+
+// app.use(
+//   OpenApiValidator.middleware({
+//     apiSpec: "../openapi/internalapi/openapi-bundled.yaml",
+//     // apiSpec: "../openapi/internalapi/openapi.yaml",
+//     validateRequests: true, // (default)
+//     validateResponses: true, // false by default
+//   }),
+// );
 
 app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/internal/v1/users/hoge", (req, res) => {
+  type T = MyTypes.external["schemas/User.yaml"];
+  const ss: T = {};
+
   res.send("Hello World!");
 });
 
