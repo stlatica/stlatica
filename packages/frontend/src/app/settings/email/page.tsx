ca21877f0d29490681450d19065dd9e44bba9295
nobuchi_home <hanazawa9to13ctrl@gmail.com>
2024-03-24 05:12:30 +0900

    settings_3階層を2階層の上書きで表示

---
+++ packages/frontend/src/app/settings/email/page.tsx
@@ -0,0 +1,12 @@
+import { MdArrowBack } from "react-icons/md";
+
+export default function Void() {
+  return (
+    <div>
+      <a href="/settings/account">
+        <MdArrowBack />
+      </a>
+      <div>メールアドレス編集画面</div>
+    </div>
+  );
+}
---
+++ packages/frontend/src/app/settings/muted_user/page.tsx
@@ -0,0 +1,12 @@
+import { MdArrowBack } from "react-icons/md";
+
+export default function Void() {
+  return (
+    <div>
+      <a href="/settings/display">
+        <MdArrowBack />
+      </a>
+      <div>ミュートするユーザの指定画面</div>
+    </div>
+  );
+}
