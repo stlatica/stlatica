dc10552d4def174fd46637f428fdeaf457b5f793
polyester <56536708+polyester-CTRL@users.noreply.github.com>
2024-03-29 22:37:56 +0900

    テーブル名を変更、user_idを固定長、外部キーとして定義

--- packages/backend/db/migrations/20240324205000_add_user_password.sql
+++ packages/backend/db/migrations/20240324205000_add_user_password.sql
@@ -1,9 +1,10 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE `user_password` (
-  `user_id`     varchar(64) NOT NULL COMMENT 'UserID',
+CREATE TABLE `user_authentication` (
+  `user_id`     char(64) NOT NULL COMMENT 'UserID',
   `user_password`   varchar(64) NOT NULL COMMENT 'UserPassword',
-  PRIMARY KEY(`user_id`)
+  PRIMARY KEY(`user_id`),
+  FOREIGN KEY (`user_id`) REFERENCES `actors` (`actor_id`)
 )
 DEFAULT CHARACTER SET utf8mb4
 DEFAULT COLLATE utf8mb4_ja_0900_as_cs
