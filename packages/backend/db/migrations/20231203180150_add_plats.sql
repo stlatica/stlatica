5b921b56f01112513d3909da85ff7cd0c4c0565f
cotton <chinoknct@gmail.com>
2024-03-31 20:08:52 +0900

    Change db schema from actors to users

--- packages/backend/db/migrations/20231203180150_add_plats.sql
+++ packages/backend/db/migrations/20231203180150_add_plats.sql
@@ -1,11 +1,12 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE `plats` (
-  `plat_id`    varchar(64) NOT NULL COMMENT 'ulid',
-  `actor_id`   varchar(64) NOT NULL COMMENT 'actor_id',
+  `plat_id`    char(26) NOT NULL COMMENT 'ulid',
+  `user_id`    char(26) NOT NULL COMMENT 'user_id',
   `content`    varchar(255) NOT NULL COMMENT 'body text',
   `created_at` bigint UNSIGNED NOT NULL COMMENT 'Unix time',
-  PRIMARY KEY (`plat_id`)
+  PRIMARY KEY (`plat_id`),
+  CONSTRAINT `fk_plats_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
 )
 DEFAULT CHARACTER SET utf8mb4
 DEFAULT COLLATE utf8mb4_ja_0900_as_cs
