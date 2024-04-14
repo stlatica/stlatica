b6ab1f021bc3beb8fb7111c21566ee31c8c4a9ee
cotton <chinoknct@gmail.com>
2024-04-14 18:12:00 +0900

    Add method to unixtime type

--- packages/backend/app/domains/types/unix_time.go
+++ packages/backend/app/domains/types/unix_time.go
@@ -14,6 +14,11 @@ func NewUnixTimeFromTime(time time.Time) UnixTime {
 	return UnixTime(time.Unix())
 }
 
+// ConvertToTime converts UnixTime to time.Time.
+func (ut UnixTime) ConvertToTime() time.Time {
+	return time.Unix(int64(ut), 0)
+}
+
 // Value implements driver.Valuer interface.
 func (ut UnixTime) Value() (driver.Value, error) {
 	return int64(ut), nil
