c5805e6928e9cf4d0c09b0d3a43fbb91e2fcfed9
cotton <chinoknct@gmail.com>
2024-05-12 12:35:32 +0900

    Fix the code to match the generated code

--- packages/backend/app/repositories/dao/user_dao.go
+++ packages/backend/app/repositories/dao/user_dao.go
@@ -30,7 +30,7 @@ type userDAO struct {
 }
 
 func (dao *userDAO) GetUser(ctx context.Context, userID types.UserID) (*domainentities.User, error) {
-	entity, err := entities.FindUser(ctx, dao.ctxExecutor, userID)
+	entity, err := entities.FindUserBase(ctx, dao.ctxExecutor, userID)
 	if err != nil {
 		return nil, err
 	}
@@ -40,7 +40,7 @@ func (dao *userDAO) GetUser(ctx context.Context, userID types.UserID) (*domainen
 
 func (dao *userDAO) GetUserByPreferredUserID(ctx context.Context,
 	preferredUserID string) (*domainentities.User, error) {
-	query := entities.UserWhere.PreferredUserID.EQ(preferredUserID)
+	query := entities.UserBaseWhere.PreferredUserID.EQ(preferredUserID)
 	entity, err := entities.Users(query).One(ctx, dao.ctxExecutor)
 	if err != nil {
 		return nil, err
@@ -49,15 +49,17 @@ func (dao *userDAO) GetUserByPreferredUserID(ctx context.Context,
 	return convertUserEntityToDomainEntity(entity), nil
 }
 
-func convertUserEntityToDomainEntity(entity *entities.User) *domainentities.User {
+func convertUserEntityToDomainEntity(entity *entities.UserBase) *domainentities.User {
 	return &domainentities.User{
-		UserID:            entity.UserID,
-		PreferredUserID:   entity.PreferredUserID,
-		PreferredUserName: entity.PreferredUserName,
-		RegisteredAt:      entity.RegisteredAt,
-		IsPublic:          entity.IsPublic,
-		MailAddress:       entity.MailAddress,
-		CreatedAt:         entity.CreatedAt,
-		UpdatedAt:         entity.UpdatedAt,
+		UserBase: domainentities.UserBase{
+			UserID:            entity.UserID,
+			PreferredUserID:   entity.PreferredUserID,
+			PreferredUserName: entity.PreferredUserName,
+			RegisteredAt:      entity.RegisteredAt,
+			IsPublic:          entity.IsPublic,
+			MailAddress:       entity.MailAddress,
+			CreatedAt:         entity.CreatedAt,
+			UpdatedAt:         entity.UpdatedAt,
+		},
 	}
 }
