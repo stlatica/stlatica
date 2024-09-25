ba1752cbd05840c3c03e71671c7d24cda4839d76
polyester <56536708+polyester-CTRL@users.noreply.github.com>
2024-09-25 23:56:41 +0900

    参照渡しから値渡しに変更

--- packages/backend/app/controllers/internalapi/v1/handler.go
+++ packages/backend/app/controllers/internalapi/v1/handler.go
@@ -59,7 +59,7 @@ func (h *handler) CreateUser(ectx echo.Context) error {
 	if err != nil {
 		return err
 	}
-	response, err := h.userController.CreateUser(ectx, *user.Name, *user.UserId, *user.Email, *user.IconImageId)
+	response, err := h.userController.CreateUser(ectx, user.Name, user.UserId, user.Email, user.IconImageId)
 	if err != nil {
 		return err
 	}
