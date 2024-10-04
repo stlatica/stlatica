76e5675cf92024ce011e5208cf67d52dd85d32aa
para7 <cometpara7@gmail.com>
2024-10-04 11:59:41 +0900

    Add SDMT ci check

--- packages/db-manager/src/routes/api/plat/random-create/+page.server.ts
+++ packages/db-manager/src/routes/api/plat/random-create/+page.server.ts
@@ -1,6 +1,6 @@
 import { getTimelineByQuery } from '$lib/orval/stlaticaInternalApi';
 import { prisma } from '$lib/prisma.server';
-import type { PageServerLoad } from '../../$types';
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
 	const users = await prisma.users.findMany({ orderBy: { preferred_user_id: 'asc' } });
