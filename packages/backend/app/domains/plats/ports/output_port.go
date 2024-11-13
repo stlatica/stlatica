12f4a3262be145d1ba4b9e6017135d30e08d5fa1
polyester <56536708+polyester-CTRL@users.noreply.github.com>
2024-11-13 22:52:52 +0900

    OutPortに変更

--- packages/backend/app/domains/plats/ports/output_port.go
+++ packages/backend/app/domains/plats/ports/output_port.go
@@ -7,8 +7,8 @@ import (
 	"github.com/stlatica/stlatica/packages/backend/app/domains/types"
 )
 
-// PlatCreateInPort is the interface for posting plat.
-type PlatCreateInPort interface {
+// PlatCreateOutPort is the interface for posting plat.
+type PlatCreateOutPort interface {
 	// CreatePlat returns Plat.
 	CreatePlat(ctx context.Context, UserID types.UserID, content string) (*entities.Plat, error)
 }
