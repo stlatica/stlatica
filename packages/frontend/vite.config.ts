2906668b2380fba430fc1cd355da231f5bae6c22
para7 <cometpara7@gmail.com>
2024-12-04 12:43:44 +0000

    [add] hono openapi 動作OK

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -3,6 +3,7 @@ import { installGlobals } from "@remix-run/node";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import serverAdapter from 'hono-remix-adapter/vite'
 
 installGlobals();
 
@@ -11,6 +12,9 @@ export default defineConfig({
     remix({
       appDirectory: "./src",
       ignoredRouteFiles: ["**/."],
+    }),
+    serverAdapter({
+      entry: './src/server/index.ts',
     }),
     tsconfigPaths(),
     vanillaExtractPlugin(),
