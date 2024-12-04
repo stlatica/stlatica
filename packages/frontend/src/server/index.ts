4b4f1b6635e268311fa12334a781886e0963ba0c
para7 <cometpara7@gmail.com>
2024-12-04 13:26:58 +0000

    [add] openapi の定義を吐けるようにした

--- packages/frontend/src/server/index.ts
+++ packages/frontend/src/server/index.ts
@@ -1,7 +1,52 @@
 import { Hono } from "hono";
+import { describeRoute } from "hono-openapi";
+import { resolver } from "hono-openapi/valibot";
+import * as v from "valibot";
+import { openAPISpecs } from "hono-openapi";
+import { apiReference } from "@scalar/hono-api-reference";
 
 const app = new Hono();
 
-app.get("/hono", (c) => c.text("Hono!"));
+const responseSchema = v.string();
 
-export default app;
+app.get(
+  "bff/hono",
+
+  describeRoute({
+    description: "Say hello to the user",
+    responses: {
+      200: {
+        description: "Successful response",
+        content: {
+          "text/plain": { schema: resolver(responseSchema) },
+        },
+      },
+    },
+  }),
+  (c) => c.text("Hono!"),
+);
+
+const baseEndpoint = "/bff";
+
+app.get(
+  "bff/openapi",
+  openAPISpecs(app, {
+    documentation: {
+      info: { title: "Hono API", version: "1.0.0", description: "Greeting API" },
+      servers: [{ url: "http://localhost:3000", description: "Local Server" }],
+    },
+  }),
+);
+
+app.get(
+  "bff/docs",
+  apiReference({
+    theme: "saturn",
+    spec: { url: "/bff/openapi" },
+  }),
+);
+
+export const honoApp = new Hono({ strict: false }).route("/", app);
+export type AppType = typeof honoApp;
+
+export default honoApp;
