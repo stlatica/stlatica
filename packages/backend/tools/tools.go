e58e61bead42371ad1d23892572e08f245648e98
polyester-CTRL <56536708+polyester-CTRL@users.noreply.github.com>
2025-04-12 18:29:35 +0000

    format code

--- packages/backend/tools/tools.go
+++ packages/backend/tools/tools.go
@@ -6,11 +6,11 @@ package tools
 // バージョン追跡を行いたいgoベースのツールをこのインポートステートメントに記載する
 // https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
 import (
+	_ "github.com/go-playground/validator/v10"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen"
 	_ "github.com/pressly/goose/v3/cmd/goose"
 	_ "github.com/volatiletech/sqlboiler/v4"
 	_ "github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-mysql"
 	_ "golang.org/x/tools/cmd/goimports"
-	_ "github.com/go-playground/validator/v10"
 )
